2a5cffa3a86c465328217d8cb40daba796bda201
tianchen zhong <tczhong24@gmail.com>
2018-06-18 12:29:25 -0500

    delete comment

--- backend/codeception/src/main/java/com/walmart/app/backend/GetService.java
+++ backend/codeception/src/main/java/com/walmart/app/backend/GetService.java
@@ -8,11 +8,15 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
 import com.microsoft.azure.documentdb.Database;
 import com.microsoft.azure.documentdb.Document;
 import com.microsoft.azure.documentdb.DocumentClient;
 import com.microsoft.azure.documentdb.DocumentClientException;
 import com.microsoft.azure.documentdb.DocumentCollection;
+import com.microsoft.azure.documentdb.RequestOptions;
+
 import org.json.JSONObject;
 
 public class GetService extends HttpServlet {
@@ -21,8 +25,16 @@ public class GetService extends HttpServlet {
    *
    */
   private DocumentClient client;
+  
+  private static Gson gson = new Gson();
+  private static String collectionLink = "";
   public GetService() {
+    
     client = new DocumentClient(AccountCredentials.HOST,AccountCredentials.MASTER_KEY,null,null);
+    RequestOptions options = new RequestOptions();
+    options.setOfferThroughput(400);
+    collectionLink = String.format("/dbs/%s/colls/%s", AccountCredentials.databaseId, AccountCredentials.collectionId);
+        
   }
 
   @Override
@@ -40,7 +52,9 @@ public class GetService extends HttpServlet {
   private Record retrievePostData(BufferedReader r){
     return null;
   }
+  
   private void insertData(DocumentClient client, Record r){
+    Document newRecord = new Document(gson.toJson(r));
 
   }
 }

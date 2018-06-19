df78d31ff738782268a1a6e31de1ab7df5190dbd
tianchen zhong <tczhong24@gmail.com>
2018-06-18 23:09:48 -0500

    update 50 sql

---
+++ backend/codeception/run.sh
@@ -0,0 +1,2 @@
+docker build . -t tczhong24/backend:0.6
+docker run -p 3000:80 tczhong24/backend:0.6
--- backend/codeception/src/main/java/com/walmart/app/backend/GetService.java
+++ backend/codeception/src/main/java/com/walmart/app/backend/GetService.java
@@ -3,6 +3,7 @@ package com.walmart.app.backend;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.MessageFormat;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
@@ -68,7 +69,9 @@ public class GetService extends HttpServlet {
 
   private JSONObject getResult(Record r) {
     JSONObject obj = new JSONObject();
-    List<Document> documentList = client.queryDocuments(collectionLink, "SELECT * FROM root r WHERE r.x < 124", null)
+    //x lan y lon
+    String SQLquery = MessageFormat.format("SELECT * FROM root dest WHERE (dest.y between ({1}-50/abs(cos(radians({0}))*69)) and ({1}+50/abs(cos(radians({0}))*69))) and (dest.x between ({0}-(50/69)) and ({0}+(50/69))) and 3956 * 2 * ASIN(SQRT( POWER(SIN(({0} -dest.x) * pi()/180 / 2), 2) +COS({0}* pi()/180) * COS(dest.x * pi()/180) *POWER(SIN(({1} -dest.y) * pi()/180 / 2), 2))) < 50",r.x,r.y);
+    List<Document> documentList = client.queryDocuments(collectionLink, SQLquery, null)
         .getQueryIterable().toList();
 
     if (documentList.size() > 0) {
